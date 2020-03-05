4cdc2b84c39f62204673444c0391099d613b80d7
Danny Morabito <25923265+thelmuxkriovar@users.noreply.github.com>
2020-03-05 09:12:58 +0100

    Moved old .bin here

--- zsh/07-oneliners.zsh
+++ zsh/07-oneliners.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 rmFolderWithProgress() { rm -rv $1 | pv -l -s $(du -a $1 | wc -l) > /dev/null }
 vf() { fzf | xargs -r -I % $EDITOR % ;}
+syncProgress() { watch -n .5 -d grep -e Dirty: -e Writeback: /proc/meminfo }
+reverseDictionary() { dictionary -r $* }
 [[ -r "/usr/share/z/z.sh" ]] && source /usr/share/z/z.sh
